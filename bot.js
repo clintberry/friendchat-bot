ab7bcea060a172e1b8e9628b0510eb04b19d69c8
Clint Berry <clint@system-11.com>
2013-10-24 14:26:05 -0600

    Added initial espn module

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 var Domo = require('domo-kun');
 var nconf = require('nconf');
 var http = require('http');
+var reddit = require('redwrap');
+var request = require('')
+
 
 nconf.argv()
      .env()
@@ -29,8 +32,58 @@ domo.route('Hello hoggle2', function(res) {
 });
 
 domo.route('hoggle2 die', function(res) {
-   this.say(res.channel, 'Go suck a wang... I am here to stay.');
+   this.say(res.channel, 'Go suck a wang, ' + res.nick + '... I am here to stay.');
 });
+
+domo.route('!reddit hot', function(res) {
+  var self = this;
+  console.log(res);
+  reddit.list('hot', function(err, data, response){
+      console.log(data); //object representing the front page of reddit w/ 'hot' filter
+      var i = 0;
+      data.data.children.forEach(function(row) {
+        if(i<5) {
+          self.say(res.channel, row.data.title + ' - ' + row.data.url); 
+        }
+        i++;
+      });
+  });
+});
+
+domo.route('!espn :team', function(res) {
+  var self = this;
+  var utahId = 254;
+  var byuId = 252;
+  //var jazzId = ;
+
+  var url = 'http://api.espn.com/v1/sports/';
+
+  var team = res.params.team;
+  var sport = null;
+
+  if(team.toLowerCase() == 'byu') {
+    url = url + 'football/college-football/teams/252';
+    sport = 'college-football';
+  }
+
+  if(team.toLowerCase() == 'utah') {
+    url = url + 'football/college-football/teams/254';
+    sport = 'college-football';
+  }
+
+  url = url + '?apikey=' + nconf.get('espnkey');
+
+  request(url, function(err, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.stringify(body);
+      info.sports[0].leagues[0].teams[0].name
+      self.say(info.sports[0].leagues[0].teams[0].name + " - " + info.sports[0].leagues[0].teams[0].record.summary);
+    }
+  });
+
+});
+
+
 
 domo.connect();
 
