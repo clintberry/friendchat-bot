2fb1ad2f751a8494e41ac316d98a9862074bb124
Clint Berry <clint@system-11.com>
2013-11-04 16:06:52 -0700

    Try a different web address to keep this sucker humming?

--- bot.js
+++ bot.js
@@ -115,6 +115,6 @@ domo.connect();
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.write('Hi. I\'m Hoggle');
   res.end();
 }).listen( process.env.PORT || 5000);
